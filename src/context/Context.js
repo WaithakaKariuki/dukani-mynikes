f8e4729f64f0b4450ca3c6a527b28ba024c345c3
NirvanaSatori <morris24kariuki@gmail.com>
2023-09-10 01:43:30 +0300

    fetch data from chec/commerce.js API

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,6 +1,7 @@
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useReducer, useState, useEffect } from "react";
 import faker from "faker";
 import { cartReducer, productReducer } from "./Reducers";
+import { commerce } from '../lib/commerce'
 // import { productsArray } from "../data/data";
 // import {useState, useEffect} from "react"
 // import {products} from "./src./App.js"
@@ -29,15 +30,15 @@ faker.seed(99);
 
 
 const Context = ({ children }) => {
-  const products = [...Array(20)].map(() => ({
-    id: faker.datatype.uuid(),
-    name: faker.commerce.productName(),
-    price: faker.commerce.price(),
-    image: faker.random.image(),
-    inStock: faker.random.arrayElement([0, 3, 5, 6, 7]),
-    fastDelivery: faker.datatype.boolean(),
-    ratings: faker.random.arrayElement([1, 2, 3, 4, 5]),
-  }));
+  // const products = [...Array(20)].map(() => ({
+  //   id: faker.datatype.uuid(),
+  //   name: faker.commerce.productName(),
+  //   price: faker.commerce.price(),
+  //   image: faker.random.image(),
+  //   inStock: faker.random.arrayElement([0, 3, 5, 6, 7]),
+  //   fastDelivery: faker.datatype.boolean(),
+  //   ratings: faker.random.arrayElement([1, 2, 3, 4, 5]),
+  // }));
 // console.log(productsArray)
 
 // const [ products, setProductArray ] = useState([]);
@@ -50,9 +51,17 @@ const Context = ({ children }) => {
 //   })();
 // }, []);
  
+const [products, setProducts] = useState([])
 
 
+const fetchProducts = async () => {
+  const { data } = await commerce.products.list()
+  setProducts(data)
+}
 
+useEffect(() => {
+  fetchProducts()
+}, [])
 
 
 // const products=productsArray
