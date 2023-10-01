1ba8a50db630dd569442bca6ca9eca8ec5ac4130
NirvanaSatori <morris24kariuki@gmail.com>
2023-10-01 20:15:29 +0300

    remove faker data

--- src/context/Context.js
+++ src/context/Context.js
@@ -9,16 +9,6 @@ faker.seed(99);
 
 
 const Context = ({ children }) => {
-  // const products = [...Array(20)].map(() => ({
-  //   id: faker.datatype.uuid(),
-  //   name: faker.commerce.productName(),
-  //   price: faker.commerce.price(),
-  //   image: faker.random.image(),
-  //   inStock: faker.random.arrayElement([0, 3, 5, 6, 7]),
-  //   fastDelivery: faker.datatype.boolean(),
-  //   ratings: faker.random.arrayElement([1, 2, 3, 4, 5]),
-  // }));
-console.log(products)
 
 
   const [state, dispatch] = useReducer(cartReducer, {
